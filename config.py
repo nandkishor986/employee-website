bb2e7367dd32a74bababf4bd94b29c84caa3728c
nandkishor986 <metangenandkishor2805@gmail.com>
2025-10-18 08:49:14 +0530

    Employee Website Deployment Code is added to the Github V1

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employeedb.cwzoicqeacft.us-east-1.rds.amazonaws.com" # RDS Endpoint
+customuser = "nandkishor986"
+custompass = "1A2b3c4d5E$%^"
+customdb = "employee"
+custombucket = "addemployee124"
+customregion = "us-east-1"
